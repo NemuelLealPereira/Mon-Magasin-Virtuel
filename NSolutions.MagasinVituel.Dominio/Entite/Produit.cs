b40ecaaee425c06cea018a0c562d81a64db1ace9
Nemuel Leal Pereira <Nemolp_15@hotmail.com>
2018-03-19 22:18:36 -0400

    Classe Produit

---
+++ NSolutions.MagasinVituel.Dominio/Entite/Produit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSolutions.MagasinVituel.Dominio.Entite
+{
+    class Produit
+    {
+        public int IdProduit { get; set; }
+        public string Nom { get; set; }
+        public string Descrition { get; set; }
+        public decimal Prix { get; set; }
+        public string Categorie { get; set; }
+    }
+}
